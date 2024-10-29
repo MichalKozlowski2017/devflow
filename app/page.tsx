b07dcab478cc7204076647747ff92b97c427144b
Michal Kozlowski <michal@Mac.lan>
2024-10-29 21:40:49 +0100

    Eslint & Prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
-const Home = () => <h1 className="text-3xl text-violet-700 font-black">Hello world</h1>;
+const Home = () => <h1 className="text-3xl font-black text-violet-700">Hello world</h1>;
 
 export default Home;
